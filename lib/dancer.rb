6583db29a28dc19295300fecef903b60efd778bf
Kevin Hayden <kevin.fhayden@gmail.com>
2019-10-22 15:56:04 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
@@ -5,6 +6,13 @@ class Dancer
   
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+=======
+require_relative './class_methods_module.rb'require_relative './dance_module.rb'
+class Dancer
+  
+  extend MetaDancing
+  include Dance
+>>>>>>> 51bc289c21ed9d4fd7e5af93eb7676bebd6c039d
   
   attr_accessor :name
   
--- lib/kid.rb
+++ lib/kid.rb
@@ -3,8 +3,13 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 class Kid
   
+<<<<<<< HEAD
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+=======
+  extend MetaDancing
+  include Dance
+>>>>>>> 51bc289c21ed9d4fd7e5af93eb7676bebd6c039d
   
   attr_accessor :name
   
